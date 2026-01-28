4792549017d2c1ff4f342924cecc36c4f1a5c311
ixsnehith <snehith.boreddy@innovatechs.com>
2026-01-28 12:20:39 +0530

    Update Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,26 +1,17 @@
-// Import required java libraries
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-// Extend HttpServlet class
 public class Hello extends HttpServlet {
 
-   private String message;
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
 
-   public void init() throws ServletException {
-      // Do required initialization
-      message = "Hello World: src/main/java/Hello.java";
-   }
-
-   public void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-
-      // Set response content type
-      response.setContentType("text/html");
-
-      // Actual logic goes here.
-      PrintWriter out = response.getWriter();
-      out.println("<h1>" + message + "</h1>");
-   }
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello from Tomcat 11 + Java 21!</h1>");
+    }
 }
+
+
